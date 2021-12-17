864c8d0e50a675c62f7f195488d6d192d1fedbd1
lagarve <miguela.hernandez@udea.edu.co>
2021-12-17 14:17:21 -0500

    function button part 1

--- app/src/main/java/com/example/nextbook/MainActivity.kt
+++ app/src/main/java/com/example/nextbook/MainActivity.kt
@@ -11,13 +11,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val infoTextView : TextView = findViewById(R.id.info_text_view)
-        val titleBookEditText : EditText = findViewById(R.id.title_edit_text)
+        /*TextView*/
+        val infoTextView        : TextView = findViewById(R.id.info_text_view)
+        /*Edit text*/
+        val titleBookEditText   : EditText = findViewById(R.id.title_edit_text)
+        val authorBookEditText  : EditText = findViewById(R.id.author_tedit_text)
+        val pagesBookEditText   : EditText = findViewById(R.id.pages_edit_text)
+        val resumenBookEditText : EditText = findViewById(R.id.resumen_edit_text)
+        /*Button*/
         val saveButton : Button = findViewById(R.id.save_button)
 
         saveButton.setOnClickListener{
             val titleBook : String = titleBookEditText.text.toString()
-            infoTextView.text = titleBook
+            val authorBook : String = authorBookEditText.text.toString()
+            val pagesBook : String = pagesBookEditText.text.toString()
+            val resumenBook : String = resumenBookEditText.text.toString()
+
+            infoTextView.text = "Titulo: $titleBook\nAutor: $authorBook\nPaginas: $pagesBook\nResumen: $resumenBook"
         }
     }
 }
